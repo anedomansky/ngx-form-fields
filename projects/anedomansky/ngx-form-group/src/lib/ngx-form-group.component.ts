4191095555dfebcc5955feea7cde652014ef3b12
anedomansky <nedomanskya@outlook.com>
2023-05-19 17:36:34 +0200

    Fixed ESLint

--- projects/anedomansky/ngx-form-group/src/lib/ngx-form-group.component.ts
+++ projects/anedomansky/ngx-form-group/src/lib/ngx-form-group.component.ts
@@ -1,18 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-ngx-form-group',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      ngx-form-group works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>ngx-form-group works!</p> `,
+  styles: [],
 })
-export class NgxFormGroupComponent {
-
-}
+export class NgxFormGroupComponent {}
